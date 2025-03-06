5a860ecd25b4ab893b02bdb6b0be25d1ccc1e44b
“prasad5083” <deviprasad5083@gmail.com>
2025-03-06 10:53:37 +0530

    auto push

---
+++ 01-shebang.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## start of the line in any script should be a ahebang notation.
+
+## Apart from the first line, none of the lines are considered as sheband
+
+## using the shebang notation, we are telling the system the interpretor to use to run this script.
+
+## your server would have java, go , python, nodejs. shell installed.
+
+## now if you run the script, how system will come to know which interpretor it has to use, that's where shebang notions helps
+
+### how to run linux bash script ?
+
+## . bash scriptname.sh
+## . sh scriptname.sh
+## ./scriptname
+
+echo welcome to the devops batch54
