ec26e8cc40e0d4e0771f10cb085d010412598d89
“prasad5083” <deviprasad5083@gmail.com>
2025-03-06 11:36:40 +0530

    auto push

---
+++ 02-colout.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# each and every colour you see on terminal will have the colour code and we need to use that code on our need.
+
+# Color	Foreground Code	Background Code
+# Black	30	40
+#Red	31	41
+##Green	32	42
+#Yellow	33	43
+#Blue	34	44
+#Magenta	35	45
+#Cyan	36	46
+#Light Gray	37	47
+#Gray	90	100
+#Light Red	91	101
+#Light Green	92	102
+#Light Yellow	93	103
+#Light Blue	94	104
+#Light Magenta	95	105
+#Light Cyan	96	106
+#White	97	107
+
+echo -e "\e [32m i am printing the green colour \e[0m"
