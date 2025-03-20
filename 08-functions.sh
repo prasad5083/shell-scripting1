7f1a4348b1a674f75551d5e66c71370af0b42f05
“prasad5083” <deviprasad5083@gmail.com>
2025-03-20 18:28:16 +0530

    auto push

--- 08-functions.sh
+++ 08-functions.sh
@@ -6,4 +6,17 @@ echo "i am ececuting the sample function"
 echo "sample function is executed"    
 }
 # how to exute the sample function
+
 sample
+
+status(){
+
+echo good morining todays date is 30march2025
+todays_date=$(date +%D)
+echo -e "good morning and todays date is \e[32;96m $todays_date \e[0m"
+
+number_of_session=$(who | wc -l)
+echo -e "number ofthe session are \e[32,93m $number_of_session \e[0m"
+}
+#caliing the status
+status
