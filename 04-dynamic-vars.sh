f54b26711e53dcc30642329ec6a04b161d304223
“prasad5083” <deviprasad5083@gmail.com>
2025-03-20 15:58:18 +0530

    auto push

--- 04-dynamic-vars.sh
+++ 04-dynamic-vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 echo good morining todays date is 30march2025
-todays_date=(date +%D)
+todays_date=$(date +%D)
 echo -e "good morning and todays date is \e[32m;43 $todays_date \e[0m"
