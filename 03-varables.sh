3dc9cb11ddadef563237cb93e36343764e1671c0
“prasad5083” <deviprasad5083@gmail.com>
2025-03-20 14:45:26 +0530

    auto push

--- 03-varables.sh
+++ 03-varables.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# a=10
+# name=dev
+# batch=54
+# topic=sheelscripting
+
+# In bash there is not concept of variables by  default everthing is the sting.
+
+# this is how we declare a veriable
 a=10
-name=dev
-batch=54
-topic=sheelscripting
+b=20
+c=30
+
+echo $a 
+echo value of the variable a is $a 
