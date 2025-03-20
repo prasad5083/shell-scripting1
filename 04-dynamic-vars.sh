e3a3f637f1864b65faba387219940d18f1728279
“prasad5083” <deviprasad5083@gmail.com>
2025-03-20 17:08:17 +0530

    auto push

--- 04-dynamic-vars.sh
+++ 04-dynamic-vars.sh
@@ -4,3 +4,5 @@ echo good morining todays date is 30march2025
 todays_date=$(date +%D)
 echo -e "good morning and todays date is \e[32;96m $todays_date \e[0m"
 
+number_of_session=$(who | wc -l)
+echo -e "number ofthe session are \e[32,96m $number_of_session \e[0m]"
