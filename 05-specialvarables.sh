afdc0e32743a1d9a9d6987e1f2af41be716c452d
“prasad5083” <deviprasad5083@gmail.com>
2025-03-20 17:32:56 +0530

    auto push

--- 05-specialvarables.sh
+++ 05-specialvarables.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo $0
-echo name of the scrip which is executed is $0
+echo "name of the scrip which is executed is $0"
+echo "name the traneris $1"
+echo "name of the cource is $2"
+echo "topic is $3"
