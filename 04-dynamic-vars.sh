b4eb9f7e7e4b33a1c4ed9d9eb923d0e59a4b6e58
“prasad5083” <deviprasad5083@gmail.com>
2025-03-20 16:04:51 +0530

    auto push

--- 04-dynamic-vars.sh
+++ 04-dynamic-vars.sh
@@ -3,4 +3,5 @@
 echo good morining todays date is 30march2025
 todays_date=$(date +%D)
 echo -e "good morning and todays date is \e[32m $todays_date \e[0m"
-echo -e "\e[32m;43m hi i am devi prasad \e[0m"
+echo -e "\e[32m;96m hi i am devi prasad \e[0m"
+echo -e "\e[44;96m i am print backgrounf as blue and foreground colour as light cyan \e[0m"
