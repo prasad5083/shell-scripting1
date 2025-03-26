1a885276c8a38cabecc5a2320f7a55dfcf78d2c6
“prasad5083” <deviprasad5083@gmail.com>
2025-03-26 15:03:39 +0530

    auto push

--- 11-case.sh
+++ 11-case.sh
@@ -4,15 +4,19 @@ Action=$1
 case $Action in
    Start)
      echo -e "\e[32m  starting the service \e[0m"
+     exit 0
      ;;
    stop)
      echo -e "\e[34m stoping the service \e[0m"
+     exit 1
      ;;
    restart)
      echo -e  "\e[35m restartinf\g the service \e[0m"
+     exit 2
      ;;
    *)
      echo -e "\e[36m possible valuse are only start or stop or restart \e[0m"
+     exit 3
      ;;
 esac   
  
