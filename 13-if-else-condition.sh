aa258b1264afcf052c1cdcf17215b7cf0d6a9cc2
“prasad5083” <deviprasad5083@gmail.com>
2025-03-26 15:46:31 +0530

    auto push

--- 13-if-else-condition.sh
+++ 13-if-else-condition.sh
@@ -3,4 +3,6 @@
 Action=$1
 if  [ "$Action" == "start" ]; then 
      echo -e "\e[32m  starting the service \e[0m"
+else 
+    echo -e "\e[36m possible valuse are only start or stop or restart \e[0m"     
 fi
